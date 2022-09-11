d5eafda9bd7c0200a215618bfbf41f9677d36efe
mkara <mkara@MERT>
2022-09-11 13:37:21 +0300

    Add project files.

---
+++ EFCore.DataBaseFirst/DAL/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore.DataBaseFirst.DAL
+{
+    public class AppDbContext:DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=MERT;Initial Catalog=EFCoreDateBaseFirstDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+
+        }
+    }
+}
---
+++ EFCore.DataBaseFirst/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EFCore.DataBaseFirst.DAL;
+using Microsoft.EntityFrameworkCore;
+
+using (var _context=new AppDbContext())
+{
+var products = await _context.Products.ToListAsync();
+    products.ForEach(p =>
+    {
+        Console.WriteLine($"{p.Id}:{p.Name}");
+    });
+}
