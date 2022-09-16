1e33a9c95e1e928423c483a6453ac60e34461e27
mkara <mkara@MERT>
2022-09-16 17:31:22 +0300

    ChangeTracker

--- EFCore.CodeFirst/Program.cs
+++ EFCore.CodeFirst/Program.cs
@@ -7,24 +7,38 @@ Initializer.Build();
 
 using (var _context = new appDbContext())
 {
-   // var product=_context.Products.First(x=>x.Id==5);
-    _context.Update(new Product { Name = "Defter", Price = 500, Stock = 500, Barcode = 500 });
-   //var product=await _context.Products.FirstAsync();
-   // Console.WriteLine($"ilk state:{_context.Entry(product).State}");
-   // _context.Entry(product).State = EntityState.Detached;
-   
-   // Console.WriteLine($"son state:{_context.Entry(product).State}");
-   // product.Name="kalem 2000";
-    
+
+    _context.Products.Add(new() { Name = "Silgi 1", Price = 100, Stock = 200, Barcode = 500 });
+    _context.Products.Add(new() { Name = "Silgi 2", Price = 200, Stock = 300, Barcode = 510 });
+    _context.Products.Add(new() { Name = "Silgi 3", Price = 300, Stock = 100, Barcode = 520 });
+    //Console.WriteLine($"Context Id:{_context.ContextId}");
+
+    _context.SaveChanges();
+    // var product=_context.Products.First(x=>x.Id==5);
+    // _context.Update(new Product { Name = "Defter", Price = 500, Stock = 500, Barcode = 500 });
+    //var product=await _context.Products.FirstAsync();
+    // Console.WriteLine($"ilk state:{_context.Entry(product).State}");
+    // _context.Entry(product).State = EntityState.Detached;
+
+    // Console.WriteLine($"son state:{_context.Entry(product).State}");
+    // product.Name="kalem 2000";
+
     //_context.Entry(newProduct).State = EntityState.Added;
-   // await _context.AddAsync(newProduct);
-    
-    await _context.SaveChangesAsync();
+    // await _context.AddAsync(newProduct);
+
+    //await _context.SaveChangesAsync();
     //Console.WriteLine($"SaveChance state:{_context.Entry(product).State}");
+
+
     //var products = await _context.Products.ToListAsync();
-    //products.ForEach(p =>
+
+
+   // products.ForEach(p =>
     //{
-    //    var state=_context.Entry(p).State;
-    //    Console.WriteLine($"{p.Id}:{p.Name}-{p.Price}-{p.Stock} state:{state}");
+        //p.Stock += 100;
+        //var state=_context.Entry(p).State;
+       //Console.WriteLine($"{p.Id}:{p.Name}-{p.Price}-{p.Stock} state:{state}");
+       
     //});
+ 
 }
