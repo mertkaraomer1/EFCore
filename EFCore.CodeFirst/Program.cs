a8edede7ab2cc2649964d7a220499488eae4ceaa
mkara <mkara@MERT>
2022-09-16 15:49:51 +0300

    CodeFirst(Migrations)

---
+++ EFCore.CodeFirst/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EFCore.CodeFirst;
+using EFCore.CodeFirst.DAL;
+
+using Microsoft.EntityFrameworkCore;
+Initializer.Build();
+
+using (var _context = new appDbContext())
+{
+    var products = await _context.Products.ToListAsync();
+    products.ForEach(p =>
+    {
+        Console.WriteLine($"{p.Id}:{p.Name}-{p.Price}-{p.Stock}");
+    });
+}
